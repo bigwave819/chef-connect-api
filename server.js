5b60ce64a36628713b963e205282174ecbba474a
Hirwa Tresor <waveb6133@gmail.com>
2025-12-29 16:34:48 +0200

    minor fix

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser());
 app.use(cors({
-  origin: "https://rwanda-events.vercel.app/",
+  origin: ["https://rwanda-events.vercel.app"],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
